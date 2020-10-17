81fec60e11e85262943a21764abac8f6273fbcfe
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-10-16 23:57:35 -0300

    setting controller

--- backend/src/controllers/OrfanatosController.ts
+++ backend/src/controllers/OrfanatosController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
+import orfanatosView from '../views/orfanatos_view';
+import * as Yup from 'yup';
+
 import Orfanatos from "../models/Orfanatos";
 
 export default {
@@ -7,9 +10,11 @@ export default {
     async index(request: Request, response: Response){
         const orfanatosRepository = getRepository(Orfanatos);
 
-        const orfanatos = await orfanatosRepository.find();
+        const orfanatos = await orfanatosRepository.find({
+            relations: ['images']
+        });
 
-        return response.json(orfanatos);
+        return response.json(orfanatosView.renderMany(orfanatos));
     },
 
     async show(request: Request, response: Response){
@@ -18,9 +23,11 @@ export default {
 
         const orfanatosRepository = getRepository(Orfanatos);
 
-        const orfanato = await orfanatosRepository.findOneOrFail(id);
+        const orfanato = await orfanatosRepository.findOneOrFail(id, {
+            relations: ['images']
+        });
 
-        return response.json(orfanato);
+        return response.json(orfanatosView.render(orfanato));
     },
 
     async create(request: Request, response: Response){ 
@@ -29,15 +36,41 @@ export default {
 
         const orfanatosRepository = getRepository(Orfanatos);
 
-        const orfanato = orfanatosRepository.create({
+        const requestImages = request.files as Express.Multer.File[];
+
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        })
+
+        const data = {
             name, 
             latitude,
             longitude, 
             about, 
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends,
+            images
+        }
+
+        const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required(),
+            opening_hours: Yup.string().required(),
+            open_on_weekends: Yup.boolean().required(),
+            images: Yup.array(Yup.object().shape({
+                path: Yup.string().required()
+            }))
         });
+
+        await schema.validate(data, {
+            abortEarly: false,
+        })
+
+        const orfanato = orfanatosRepository.create(data);
 
         await orfanatosRepository.save(orfanato)
 
