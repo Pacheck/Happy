60fd978b70390b1ddce485b9ad79913e5a5c6009
Pedro Pacheco <35742444+Pacheck@users.noreply.github.com>
2020-10-16 23:58:01 -0300

    adding path and cors / updating server

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,17 +1,27 @@
 import { Router } from "express";
+import multer from 'multer';
 
+import uploadConfig from './config/upload'
 import OrfanatosController from './controllers/OrfanatosController';
 
 const routes = Router();
-
+const upload = multer(uploadConfig);
 
 
 routes.get('/orfanatos', OrfanatosController.index);
 
 routes.get('/orfanatos/:id', OrfanatosController.show);
 
+routes.post('/orfanatos', upload.array('images'), OrfanatosController.create);
 
-routes.post('/orfanatos', OrfanatosController.create);
-
+// {
+// 	"name" : "Lar dos guri",
+// 	"latitude": -7.9344978,
+// 	"longitude": -14.4107581,
+// 	"about": "Sobre o orfanato",
+// 	"instructions": "venha visitar caraio",
+// 	"opening_hours": "das 9h até 00h",
+// 	"open_on_weekends": false
+// }
 
 export default routes;
